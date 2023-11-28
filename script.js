1552076babe6a5d17b1fb8b484715910ca7041e4
julian <jdrc@protonmail.com>
2023-11-28 13:24:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container')
+const submit_btn = document.querySelector('.submit-btn')
+const modal = document.querySelector('.modal')
+const dismiss_btn = document.querySelector('.dismiss-btn')
+
+submit_btn.addEventListener('click', (e) => {
+    e.preventDefault
+    modal.show()
+    // blur body or container ?
+})
+
+dismiss_btn.addEventListener('click', () => { modal.close() })
