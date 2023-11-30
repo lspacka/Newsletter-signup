d7c9a352e28cf17cda7fac897db89f18f118912d
julian <jdrc@protonmail.com>
2023-11-29 20:09:10 -0500

    layout adjustments

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const container = document.querySelector('.container')
+const form = document.querySelector('form')
+const input = document.querySelector('input')
 const submit_btn = document.querySelector('.submit-btn')
 const modal = document.querySelector('.modal')
 const dismiss_btn = document.querySelector('.dismiss-btn')
+const overlay = document.querySelector('.overlay')
+const email = document.querySelector('.success-email')
 
-submit_btn.addEventListener('click', (e) => {
+form.addEventListener('submit', e => {
     e.preventDefault()
     modal.show()
-    // blur body or container ?
+    if (input.value.includes('@')) {
+        input.classList.remove('error')
+        error.textContent = ''
+    }
+    overlay.style.display = 'block'
+    email.innerText = input.value
 })
 
-dismiss_btn.addEventListener('click', () => { modal.close() })
+submit_btn.addEventListener('click', () => {
+    // input.setCustomValidity = ''
+    if (input.value == '' || !input.value.includes('@')) {
+        form.classList.add('error')
+        error.textContent = 'Enter valid email'
+    } else {
+        input.classList.remove('error')
+        error.textContent = ''
+    }
+})
+
+dismiss_btn.addEventListener('click', () => { 
+    modal.close() 
+    overlay.style.display = 'none'
+    input.value = ''
+})
